925726dcc80077395d05675f0ef57b68e36d3df3
naoto67 <uver.com67@gmail.com>
2022-07-13 21:12:01 +0900

    バルクインサート

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -1197,23 +1197,26 @@ func postIsuCondition(c echo.Context) error {
 		return c.String(http.StatusNotFound, "not found: isu")
 	}
 
-	for _, cond := range req {
+	builder := strings.Builder{}
+	values := make([]interface{}, 0, len(req))
+	builder.WriteString("INSERT INTO `isu_condition`(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES ")
+	for i, cond := range req {
 		timestamp := time.Unix(cond.Timestamp, 0)
 
 		if !isValidConditionFormat(cond.Condition) {
 			return c.String(http.StatusBadRequest, "bad request body")
 		}
 
-		_, err = tx.Exec(
-			"INSERT INTO `isu_condition`"+
-				"	(`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`)"+
-				"	VALUES (?, ?, ?, ?, ?)",
-			jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
-		if err != nil {
-			c.Logger().Errorf("db error: %v", err)
-			return c.NoContent(http.StatusInternalServerError)
+		builder.WriteString("(?, ?, ?, ?, ?)")
+		if (len(req) - 1) != i {
+			builder.WriteString(",")
 		}
-
+		values = append(values, jiaIsuUUID, timestamp, cond.IsSitting, cond.Condition, cond.Message)
+	}
+	_, err = tx.Exec(builder.String(), values...)
+	if err != nil {
+		c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	err = tx.Commit()
